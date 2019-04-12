4f527629add2448aa469f36e1fe88c0ad87b7a2c
李静 <leejing397@gmail.com>
2019-04-12 14:53:35 +0800

    add day03

---
+++ Day03_信号量/SignalTest/SignalTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignalTest
+//
+//  Created by Iris on 2019/4/12.
+//  Copyright © 2019年 国信司南（北京）地理信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
