9eaf0834572eedc01b549711cd5a4ae831cd87b5
李静 <leejing397@gmail.com>
2019-04-25 17:41:10 +0800

    03 信号量

--- Day03_信号量/SignalTest/SignalTest/ViewController.m
+++ Day03_信号量/SignalTest/SignalTest/ViewController.m
@@ -17,9 +17,84 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self test2];
+}
+- (void)test2 {
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(1);
+    dispatch_queue_t queue = dispatch_queue_create(0, DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"执行任务一");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"网络任务一");
+            dispatch_semaphore_signal(semaphore);
+        });
+    });
+    
+    dispatch_async(queue, ^{
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"执行任务二");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"网络任务二");
+            dispatch_semaphore_signal(semaphore);
+        });
+    });
+    
+    dispatch_async(queue, ^{
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"执行任务三");
+        dispatch_semaphore_signal(semaphore);
+    });
+    
+    dispatch_async(queue, ^{
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"执行完成");
+        dispatch_semaphore_signal(semaphore);
+    });
 }
 
-
+- (void)test1 {
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"同步任务A");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"网络异步任务一");
+            dispatch_group_leave(group);
+        });
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        
+        NSLog(@"同步任务B");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.8f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"网络异步任务二");
+            dispatch_group_leave(group);
+        });
+    });
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"同步任务C");
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        
+        NSLog(@"同步任务D");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"网络异步任务四");
+            dispatch_group_leave(group);
+        });
+    });
+    
+    dispatch_group_notify(group, queue, ^{
+        NSLog(@"任务完成执行");
+    });
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
